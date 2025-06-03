a1beb2ea1f72cdee949a75cd0066bb1743742bbe
Anton Martynov <anton.martynov@simbirsoft.com>
2025-06-03 10:58:43 +0400

    1

--- components.d.ts
+++ components.d.ts
@@ -10,6 +10,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     AppFooter: typeof import('./src/components/AppFooter.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    Leagues: typeof import('./src/components/Leagues.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
